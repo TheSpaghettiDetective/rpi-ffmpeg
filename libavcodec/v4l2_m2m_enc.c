6c96dde68e7acb68fb132c83c86186e9b44d5cc0
Your Name <you@example.com>
2023-03-15 04:04:25 +0000

    Add header to shorten the initial delay

--- libavcodec/v4l2_m2m_enc.c
+++ libavcodec/v4l2_m2m_enc.c
@@ -222,7 +222,7 @@ static int v4l2_prepare_encoder(V4L2m2mContext *s)
         v4l2_set_timeperframe(s, avctx->framerate.den, avctx->framerate.num);
 
     /* set ext ctrls */
-    v4l2_set_ext_ctrl(s, MPEG_CID(HEADER_MODE), MPEG_VIDEO(HEADER_MODE_SEPARATE), "header mode", 0);
+    v4l2_set_ext_ctrl(s, MPEG_CID(HEADER_MODE), MPEG_VIDEO(HEADER_MODE_SEPARATE), "header mode", 1);
     v4l2_set_ext_ctrl(s, MPEG_CID(BITRATE) , avctx->bit_rate, "bit rate", 1);
     v4l2_set_ext_ctrl(s, MPEG_CID(FRAME_RC_ENABLE), 1, "frame level rate control", 0);
     v4l2_set_ext_ctrl(s, MPEG_CID(GOP_SIZE), avctx->gop_size,"gop size", 1);
